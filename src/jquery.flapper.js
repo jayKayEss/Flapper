8d944e760c569d9e9c4178468419f8850d8ecdb0
Justin Sheckler <self@jaykayess.com>
2012-10-10 18:58:50 -0400

    getting there; using new digit object

--- src/jquery.flapper.js
+++ src/jquery.flapper.js
@@ -6,7 +6,7 @@
         this.$ele = $ele;
         this.options = $.extend({}, this.defaults, options);
         
-        this.$div = $('<div class="flapper"><div class="stripe"></div></div>');
+        this.$div = $('<div class="flapper"></div>');
         this.$ele.hide().after(this.$div);
         
         this.$ele.change(function(){
@@ -23,177 +23,153 @@
             padding: '&nbsp;',
             commafy: false,
             prefix: null,
-            comma: ','
+            comma: ',',
+            digits: {}
         },
         
         init: function() {
-            this.$digits = [];
-            this.targets = [];
-            this.positions = [];
-            this.hidepadding = false;
-            
-            if (this.options.padding == null) {
-                this.options.padding = '&nbsp;';
-                this.hidepadding = true;
-            }
+            this.digits = [];
             
             for (i=0; i<this.options.width; i++) {
-                this.$digits[i] = $('<span class="digit"></span>').hide();
-                this.$div.prepend(this.$digits[i]);
-                this.setPadding(i);
+                this.digits[i] = new FlapDigit(null, this.options.digits);
+                this.$div.prepend(this.digits[i].$ele);
             }
 
             this.update();
         },
         
         update: function() {
-            var _this = this;
-            
-            if (this.timer) {
-                clearInterval(this.timer);
-            }
+            var value = this.$ele.val();
+            var digits = this.getDigits(value);
 
-            this.setDisplayTargets();
-            
-            this.timer = setInterval(function(){
-                var changed = false;
-                
-                for (var i=0; i<_this.options.width; i++) {
-                    var current = _this.positions[i];
-                    var target = _this.targets[i];
-                    var atzero = current == 0 || isNaN(current);
-                    
-                    if (current != target) {
-                        if (target == null && atzero) {
-                            _this.setPadding(i);
-                            
-                        } else if (isNaN(target) && atzero) {
-                            _this.setDigit(i, target);
-                            
-                        } else {
-                            _this.incrementDigit(i);
-                            changed = true;
-                        }
-                    }
-                }
-
-                if (_this.hidepadding) {
-                    for (var i=_this.options.width-1; i>=0; i--) {
-                        if (_this.positions[i] == null) {
-                            _this.$digits[i].hide();
-                        } else {
-                            break;
-                        }
-                    }
-                }
-                
-                if (!changed) {
-                    clearInterval(_this.timer);
-                }
-                
-            }, this.options.framerate);
-            
-        },
-
-        setDisplayTargets: function() {
-            var _this = this;
-            var val = Math.floor(this.$ele.val());
-            var dnew = this.getDigits(val);
-            var padout = this.options.padding != '&nbsp;' && !this.hidepadding;
-            var prefixed = false;
-            var i = 0;
-            var d = 0;
-
-            do {
-                if (d < dnew.length || padout) {
-                    if (_this.options.commafy && (i % 4 == 3)) {
-                        _this.targets[i] = _this.options.comma;
-                        continue;
-                    }
-                }
-                
-                if (d < dnew.length) {
-                    _this.targets[i] = dnew[d];
-                    d++;
-                    
-                } else {
-
-                    if (_this.options.prefix) {
-
-                        if (!prefixed && !padout) {
-                            _this.targets[i] = _this.options.prefix;
-                            prefixed = true;
-                            continue;
-                        }
-                        
-                        if (!prefixed && i == _this.options.width - 1) {
-                            _this.targets[i] = _this.options.prefix;
-                            prefixed = true;
-                            continue;
-                        }
-                        
-                    }
-
-                    if (_this.hidepadding) {
-                        _this.targets[i] = null;
-                    } else {
-                        _this.targets[i] = _this.options.padding;
-                    }
-                }
-                
-            } while (++i < _this.options.width);
-        },
-        
-        incrementDigit: function(i) {
-            var n = parseInt(this.positions[i], 10);
-            
-            if (isNaN(n)) {
-                n = 0;
-            } else {
-                n++;
-                if (n > 9) {
-                    n -= 10;
-                }
-            }
-            
-            this.$digits[i].html(n+'');
-            this.$digits[i].show();
-            this.positions[i] = n;
-        },
-        
-        setDigit: function(i, v) {
-            this.$digits[i].html(v);
-            this.$digits[i].show();
-            this.positions[i] = v;
-        },
-        
-        setPadding: function(i) {
-            this.$digits[i].html(this.options.padding);
-            
-            if (this.hidepadding) {
-                this.positions[i] = null;
-            } else {
-                this.$digits[i].show();
-                this.positions[i] = this.options.padding;
+//            console.log('START', this.$ele, digits.length, digits);
+            for (var i=0; i<this.digits.length; i++) {
+//                console.log('LOOP', i, digits[i]);
+                this.digits[i].goToChar(digits[i]);
             }
         },
-        
+
         getDigits: function(val, length) {
-            var ret = [];
-            var num = val;
+            var strval = val + '';
+            var digits = strval.split('');
 
-            if (num == 0) {
-                return [0];
-            }
+//            console.log('GETDIGITS', val, length, strval, digits);
 
-            while (num > 0) {
-                ret.push(num % 10);
-                num = Math.floor(num / 10);
-            }
-            
-            return ret;
+            // TODO: pad/truncate to correct width
+            return digits;
         },
     }
+
+    FlapDigit = function($ele, opts) {
+        this.options = $.extend({}, this.defaults, opts);
+
+        this.pos = 0;
+        this.timeout;
+        this.options.animate = true;
+
+        if (!$ele) {
+            this.$ele = $(this.htmlTemplate);
+        } else {
+            this.$ele = $ele;
+        }
+
+        this.$prev = this.$ele.find('.front.top, .back.bottom');
+        this.$next = this.$ele.find('.back.top, .front.bottom'); 
+        this.$back_top = this.$ele.find('.back.top');
+        this.$back_bottom = this.$ele.find('.back.bottom');
+        this.$front_top = this.$ele.find('.front.top');
+        this.$front_bottom = this.$ele.find('.front.bottom');
+
+        this.initialize();
+    }
+
+    FlapDigit.prototype = {
+
+        defaults: {
+            chars: ['&nbsp;', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', ',', ':', '$'],
+            framerate: 150,
+            animate: true
+        },
+
+        initialize: function() {
+            this.$prev.html(this.options.chars[0]);
+            this.$next.hide().html(this.options.chars[0]);
+        },
+
+        htmlTemplate: '<div class="digit"><div class="back top">&nbsp;</div>' +
+            '<div class="back bottom">&nbsp;</div>' +
+            '<div class="front top">&nbsp;</div>' +
+            '<div class="front bottom">&nbsp;</div></div>',
+
+        increment: function() {
+            var _this = this;
+
+            var next = this.pos + 1;
+            if (next >= this.options.chars.length) {
+                next = 0;
+            }
+
+            this.$prev.show().html(this.options.chars[this.pos]);
+            this.$next.hide().html(this.options.chars[next]);
+
+            var speed1 = Math.floor(Math.random() * this.options.framerate * .4 + this.options.framerate * .3);
+            var speed2 = Math.floor(Math.random() * this.options.framerate * .1 + this.options.framerate * .2);
+
+            if (this.options.animate) {
+                this.$back_top.show();
+                this.$front_bottom.transform({ scaleY: 0.0 });
+                this.$front_top.transform({ scaleY: 1.0 }).stop().show().animate({ scaleY: 0.0 }, speed1, 'swing', function(){
+                    _this.$front_bottom.stop().show().animate({ scaleY: 1.0 }, speed2, 'linear');
+                });
+            } else {
+                this.$front_top.hide();
+                this.$back_top.show();
+                if (this.timeout) {
+                    clearTimeout(this.timeout);
+                }
+                this.timeout = setTimeout(function(){
+                    this.$back_bottom.hide();
+                    this.$front_bottom.show();
+                }, speed);
+            }
+
+            this.pos = next;
+        },
+
+        goToPosition: function(pos) {
+            var _this = this;
+
+            if (this.options.framerate_timer) {
+                clearInterval(this.options.framerate_timer);
+                this.options.framerate_timer = null;
+            }
+
+            this.options.framerate_timer = setInterval(function(){
+                if (_this.pos == pos) {
+                    clearInterval(_this.options.framerate_timer);
+                    _this.options.framerate_timer = null;
+                } else {
+                    _this.increment();
+                }
+            }, this.options.framerate);
+        },
+
+        goToChar: function(char) {
+//            if (char =~ /^\s+$/) {
+//                char = '&nbsp;';
+//            }
+
+//            console.log(char, this.options.chars);
+            var pos = this.options.chars.lastIndexOf(char);
+            
+            if (pos == -1) {
+                pos = 0;
+            }
+
+//            console.log('GO TO CHAR', char, pos);
+            this.goToPosition(pos);
+        }
+    };
 
 	$.fn.flapper = function(options) {
         this.each(function(){
